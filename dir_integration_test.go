376092b834cd6851c60b25e013e0478d36ea6257
Abhishek Mukherjee <abhishek.mukher.g@gmail.com>
2015-02-17 20:01:51 -0500

    Add gocheck

---
+++ dir_integration_test.go
@@ -0,0 +1,79 @@
+package xdg
+
+import (
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"gopkg.in/check.v1"
+)
+
+type Suite struct {
+	tempDir                            string
+	m                                  mockEnv
+	fileInHome, fileInLocal, fileInLib *os.File
+}
+
+func Test(t *testing.T) { check.TestingT(t) }
+
+var _ = check.Suite(&Suite{})
+
+func (s *Suite) SetUpTest(c *check.C) {
+	s.tempDir = c.MkDir()
+
+	dataDirs := []string{filepath.Join(s.tempDir, "local"), filepath.Join(s.tempDir, "lib")}
+	s.m = mockEnv{
+		"XDG_DATA_HOME": filepath.Join(s.tempDir, "home"),
+		"XDG_DATA_DIRS": strings.Join(dataDirs, ":"),
+	}
+	os.Mkdir(s.m["XDG_DATA_HOME"], 0700)
+	for _, d := range dataDirs {
+		os.Mkdir(d, 0700)
+	}
+
+	var err error
+	s.fileInHome, err = os.Create(filepath.Join(s.m["XDG_DATA_HOME"], "fileInHome"))
+	if err != nil {
+		c.Fatal("Failed to create test files:", err)
+	}
+
+	s.fileInLocal, err = os.Create(filepath.Join(dataDirs[0], "fileInLocal"))
+	if err != nil {
+		c.Fatal("Failed to create test files:", err)
+	}
+
+	s.fileInLib, err = os.Create(filepath.Join(dataDirs[0], "fileInLib"))
+	if err != nil {
+		c.Fatal("Failed to create test files:", err)
+	}
+
+	for _, f := range []*os.File{s.fileInHome, s.fileInLocal, s.fileInLib} {
+		err = f.Close()
+		if err != nil {
+			c.Fatal("Failed to create test files:", err)
+		}
+	}
+
+}
+
+func (s *Suite) TestOpen2(c *check.C) {
+	readEnv := Data.fromEnvironment(s.m.Getenv)
+	runTest := func(filename string, expected *os.File) {
+		openedFile, err := readEnv.Open(filename)
+		if err != nil {
+			c.Errorf("Open(%q) = err(%q) but was expecting %q", filename, err, expected)
+			return
+		}
+		if openedFile.Name() != expected.Name() {
+			c.Errorf("Open(%q) = %q but was expected %q", filename, openedFile.Name(), expected.Name())
+		}
+	}
+	runTest("fileInHome", s.fileInHome)
+	runTest("fileInLocal", s.fileInLocal)
+	runTest("fileInLib", s.fileInLib)
+	openedFile, err := readEnv.Open("doesNotExist")
+	if err == nil {
+		c.Errorf("Found a file that doesn't exist?", openedFile.Name())
+	}
+}
